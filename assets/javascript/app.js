aae6d566ee73e9e70e5b4f21b28ccc39fceecb02
noble0423 <ryan.noble82@gmail.com>
2018-12-28 15:40:59 -0600

    countdown is working and displaying on html.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -9,19 +9,69 @@ var questions = [
     {q: "question 5", a: "answer 5"},
 ];
 
+var questionCount = 0;
 
+var questionCountdown = 20;
+var countdownRunning = false;
+var intervalId;
 
 
 // FUNCTIONS
 ////////////////////////////////////////////////////////////////////////////////////////
 function renderQuestions() {
 
-    $("#questions-div").append(questions[0].q);
-    
-    // use a for loop 
+        // if there are no more questions, stop renderQuestions function
+        if (questionCount > (questions.length - 1)) {
+            return;
+        }
 
-    // check what question we are one using an if stmt
+        else {
+
+            // TODO: check what question we are one using an if stmt before rendering the next question
+            for (let i = 0; i < questions.length; i++) {
+                $("#questions-div").text(questions[i].q);
+                console.log("count " + questionCount);
+                questionCount++;
+        }
+    };
+};
+
+function startCountdown() {
+    clearInterval(intervalId);
+    intervalId = setInterval(decrement, 1000);
+};
+
+function decrement() {
+
+    // Decrease the countdown
+    questionCountdown--;
+
+    // Place current countdown number onto html
+    $("#timer").html("<h2>" + questionCountdown + "</h2>");
+
+    // if timer is at 0, stop the countdown
+    if (questionCountdown === 0) {
+        stop();
+        // alert("time's up!");
+
+        // TODO: allow countdown to show 0, show loading circle (3 sec timeout), before restarting countdown
+        restartCountdown();
+    };
 }
+
+// function to stop the question countdown
+function stop() {
+    clearInterval(intervalId);
+};
+
+// function to reset and restart countdown
+function restartCountdown() {
+    // if (!countdownRunning) {
+    //     intervalId = setInterval(count, 1000);
+    // }
+    questionCountdown = 20;
+    $("#timer").html("<h2>" + questionCountdown + "</h2>");
+};
 
 
 
@@ -29,5 +79,6 @@ function renderQuestions() {
 // MAIN PROCESS
 ////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function() {
-    renderQuestions();
+    // renderQuestions();
+    startCountdown();
 });
