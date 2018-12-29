e4ad809a6b522ac80d8297e946e406a6a0693f36
noble0423 <ryan.noble82@gmail.com>
2018-12-28 18:18:33 -0600

    answer choices are now working in conjunction with countdwon timer and render questions function.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,11 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 
 var questions = [
-    {q: "question 1", a: "answer 1"},
-    {q: "question 2", a: "answer 2"},
-    {q: "question 3", a: "answer 3"},
-    {q: "question 4", a: "answer 4"},
-    {q: "question 5", a: "answer 5"},
+    {q: "question 1", c1: "Q1 a", c2: "Q1 b", c3: "Q1 c", c4: "Q1 d", a: "answer 1"},
+    {q: "question 2", c1: "Q2 a", c2: "Q2 b", c3: "Q2 c", c4: "Q2 d", a: "answer 2"},
+    {q: "question 3", c1: "Q3 a", c2: "Q3 b", c3: "Q3 c", c4: "Q3 d", a: "answer 3"},
+    {q: "question 4", c1: "Q4 a", c2: "Q4 b", c3: "Q4 c", c4: "Q4 d", a: "answer 4"},
+    {q: "question 5", c1: "Q5 a", c2: "Q5 b", c3: "Q5 c", c4: "Q5 d", a: "answer 5"},
 ];
 
 var questionIndex = 0;
@@ -27,17 +27,27 @@ function renderQuestions() {
         return;
     }
 
-    // else {
-
-        // TODO: check what question we are one using an if stmt before rendering the next question
-        // for (let i = 0; i < questions.length; i++) {
+    // Checks what question we are one using an if stmt before rendering the next question
 
     else if (questionIndex <= (questions.length - 1)) {
-        console.log(questionIndex);
+        // console.log(questionIndex);
+
+        // display question on html
         $("#questions-div").text(questions[questionIndex].q);
+
+        // display the corrseponding answers on html
+        var choicesDiv = $("<div>");
+        choicesDiv.append("<br>");
+
+        $("#answers-div").text("ANSWER CHOICES")
+            .append(choicesDiv)
+            .append("<span>" + questions[questionIndex].c1 + "</span><br>")
+            .append("<span>" + questions[questionIndex].c2 + "</span><br>")
+            .append("<span>" + questions[questionIndex].c3 + "</span><br>")
+            .append("<span>" + questions[questionIndex].c4 + "</span><br>");
+
         questionIndex++;
-        console.log(questionIndex);
-        // renderQuestions();
+        // console.log(questionIndex);
     }
 };
 
@@ -59,7 +69,6 @@ function decrement() {
     // if timer is at 0, stop the countdown
     if (questionCountdown === 0) {
         stopCountdown();
-        // alert("time's up!");
 
         // TODO: allow countdown to show 0, show loading circle (3 sec timeout), before restarting countdown
         restartCountdown();
@@ -79,8 +88,6 @@ function restartCountdown() {
     questionCountdown = 5;
     $("#timer").html("<h2>" + questionCountdown + "</h2>");
     startCountdown();
-    // renderQuestions();
-
 };
 
 
