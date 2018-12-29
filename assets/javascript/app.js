a603b6efd898cb6109521902cb72c97cddb93e38
noble0423 <ryan.noble82@gmail.com>
2018-12-28 18:27:57 -0600

    future TODO's have been added as notes on the app.js pg.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,11 @@
 ////////////////////////////////////////////////////////////////////////////////////////
 
 var questions = [
-    {q: "question 1", c1: "Q1 a", c2: "Q1 b", c3: "Q1 c", c4: "Q1 d", a: "answer 1"},
-    {q: "question 2", c1: "Q2 a", c2: "Q2 b", c3: "Q2 c", c4: "Q2 d", a: "answer 2"},
-    {q: "question 3", c1: "Q3 a", c2: "Q3 b", c3: "Q3 c", c4: "Q3 d", a: "answer 3"},
-    {q: "question 4", c1: "Q4 a", c2: "Q4 b", c3: "Q4 c", c4: "Q4 d", a: "answer 4"},
-    {q: "question 5", c1: "Q5 a", c2: "Q5 b", c3: "Q5 c", c4: "Q5 d", a: "answer 5"},
+    {q: "question 1", c1: "Q1 a", c2: "Q1 b", c3: "Q1 c", c4: "Q1 d", a: "Q1 a"},
+    {q: "question 2", c1: "Q2 a", c2: "Q2 b", c3: "Q2 c", c4: "Q2 d", a: "Q2 b"},
+    {q: "question 3", c1: "Q3 a", c2: "Q3 b", c3: "Q3 c", c4: "Q3 d", a: "Q3 c"},
+    {q: "question 4", c1: "Q4 a", c2: "Q4 b", c3: "Q4 c", c4: "Q4 d", a: "Q4 d"},
+    {q: "question 5", c1: "Q5 a", c2: "Q5 b", c3: "Q5 c", c4: "Q5 d", a: "Q5 a"},
 ];
 
 var questionIndex = 0;
@@ -91,6 +91,20 @@ function restartCountdown() {
 };
 
 
+
+//TODO: 
+    // Set click event for all answer choices (var userChoice)
+        // Compare userChoice to answer for that question
+            // If right, show the correct animation
+            // If wrong, show the incorrect animation and display the correct answer
+    // Style the dynamic html items (add necessary classes/id's)
+    // Write logic to move onto next question once user makes a choice
+    // Write the loading question logic (incl the "thinking" gif)
+    // Write the end game screen
+        // # Correct
+        // # Incorrect
+        // Review incorrect answers??
+    // Style the entire pg (css, bootstrap)
 
 
 // MAIN PROCESS
